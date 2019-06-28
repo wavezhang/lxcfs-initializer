a8b078187ee8f88317e9adea35deaad0ac78fb91
Yafei Bao <baoyafei@gmail.com>
2019-06-28 17:33:31 +0800

    update lxcfs image

--- deploy/lxcfs-image/start.sh
+++ deploy/lxcfs-image/start.sh
@@ -5,11 +5,12 @@ nsenter -m/proc/1/ns/mnt fusermount -u /var/lib/lxcfs 2> /dev/null || true
 nsenter -m/proc/1/ns/mnt [ -L /etc/mtab ] || \
         sed -i "/^lxcfs \/var\/lib\/lxcfs fuse.lxcfs/d" /etc/mtab
 
+# Update lxcfs
+cp -f /lxcfs/lxcfs /usr/local/bin/lxcfs
+cp -f /lxcfs/liblxcfs.so /usr/local/lib/lxcfs/liblxcfs.so
+
 # Prepare
-mkdir -p /host/var/lib/lxcfs
-mkdir -p /host/usr/local/lib/lxcfs
-cp -f /lxcfs/lxcfs /host/usr/local/bin/lxcfs
-cp -f /lxcfs/liblxcfs.so /host/usr/local/lib/lxcfs/liblxcfs.so
+mkdir -p /usr/local/lib/lxcfs /var/lib/lxcfs
 
 # Mount
 exec nsenter -m/proc/1/ns/mnt lxcfs /var/lib/lxcfs/

8a798f8b137c50034e3f05c5612eb64f0b4825ff
Yafei Bao <baoyafei@gmail.com>
2019-07-01 09:06:53 +0800

    update

--- deploy/node.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-wget https://copr-be.cloud.fedoraproject.org/results/ganto/lxd/epel-7-x86_64/00486278-lxcfs/lxcfs-2.0.5-3.el7.centos.x86_64.rpm
-yum install lxcfs-2.0.5-3.el7.centos.x86_64.rpm  
-systemctl enable lxcfs
-systemctl start lxcfs
